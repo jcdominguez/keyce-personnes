acc3b68cf6373d19840586f8062c61a963e7b43b
jcdominguez <jcd.pro@gmail.com>
2024-05-14 14:40:53 +0200

    ajout model dans Thymeleaf

--- src/main/java/com/example/personnes/PersonneMvcController.java
+++ src/main/java/com/example/personnes/PersonneMvcController.java
@@ -1,13 +1,17 @@
 package com.example.personnes;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonneMvcController {
 
     @GetMapping("helloMVC")
-    public String sayHello(){
+    public String sayHello(Model model){
+
+        String monPrenom = "Jean-Christophe";
+        model.addAttribute("prenom", monPrenom);
 
         return "hello.html";
     }
