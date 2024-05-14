b49e93a9740d472fa0fae77165b1f7094e05c4c9
jcdominguez <jcd.pro@gmail.com>
2024-05-14 14:51:42 +0200

    Thymeleaf : boucle

--- src/main/java/com/example/personnes/PersonneMvcController.java
+++ src/main/java/com/example/personnes/PersonneMvcController.java
@@ -1,11 +1,17 @@
 package com.example.personnes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PersonneMvcController {
+
+    @Autowired
+    PersonneDbService personneDbService;
 
     @GetMapping("helloMVC")
     public String sayHello(Model model){
@@ -14,5 +20,15 @@ public class PersonneMvcController {
         model.addAttribute("prenom", monPrenom);
 
         return "hello.html";
+    }
+
+    @GetMapping("personnes.html")
+    public String getPersonnes(Model model){
+
+        List<Personne> personnes = personneDbService.getAll();
+
+        model.addAttribute("personnes", personnes);
+
+        return "personnes.html";
     }
 }
