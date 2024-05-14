b3b8996253d7900b24b2e47b56412b881ddfa16f
jcdominguez <jcd.pro@gmail.com>
2024-05-14 11:31:02 +0200

    Repo Adresse

---
+++ src/main/java/com/example/personnes/AdresseRepository.java
@@ -0,0 +1,9 @@
+package com.example.personnes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdresseRepository extends JpaRepository<Adresse,Integer> {
+
+}
