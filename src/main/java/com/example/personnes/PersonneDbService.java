416bf927ef90afb753cd7b0e7c3a331fdd84eedb
jcdominguez <jcd.pro@gmail.com>
2024-05-14 13:56:03 +0200

    oublie annotation

--- src/main/java/com/example/personnes/PersonneDbService.java
+++ src/main/java/com/example/personnes/PersonneDbService.java
@@ -1,10 +1,12 @@
 package com.example.personnes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PersonneDbService implements Annuaire {
 
     @Autowired
