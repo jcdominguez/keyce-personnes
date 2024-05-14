8e1bc438483ee17b68a176086aef897be79f2b42
jcdominguez <jcd.pro@gmail.com>
2024-05-14 09:55:46 +0200

    configuration JPA

--- src/main/java/com/example/personnes/Personne.java
+++ src/main/java/com/example/personnes/Personne.java
@@ -1,10 +1,18 @@
 package com.example.personnes;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Personne {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String prenom;
     private String nom;
     //@JsonIgnore
